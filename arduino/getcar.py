c761259c925181e92293c24f4e2c551529ff9853
mahmoudSh58 <97961840+mahmoudSh58@users.noreply.github.com>
2023-03-20 19:41:25 +0200

    add getcar function

---
+++ arduino/getcar.py
@@ -0,0 +1,18 @@
+import serial
+
+ser = serial.Serial('COM7',9600)
+def getcar(park_n):
+    c= 'G'+ chr(park_n);
+    ser.write(c.encode())
+    ser.flush()
+    while True :
+        if(ser.in_waiting):
+            d = str(ser.read(1),'UTF-8')
+            if(d=='D'): break
+
+    print('done')
+
+# test function
+while True :
+    c = int(input('Enter number of parking : '))
+    getcar(c)
